8421055f5810892354b1ede3369351f71027ce03
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-05-30 07:13:02 +0200

    Added carthage support

---
+++ SubmitTransition/SubmitTransition.h
@@ -0,0 +1,19 @@
+//
+//  SubmitTransition.h
+//  SubmitTransition
+//
+//  Created by Błażej Szajrych on 30.05.2016.
+//  Copyright © 2016 Uniface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubmitTransition.
+FOUNDATION_EXPORT double SubmitTransitionVersionNumber;
+
+//! Project version string for SubmitTransition.
+FOUNDATION_EXPORT const unsigned char SubmitTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubmitTransition/PublicHeader.h>
+
+
